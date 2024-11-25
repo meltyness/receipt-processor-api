3f1ee8d94c03a3972777092aed085e04cb01b70a
meltyness <86126050+meltyness@users.noreply.github.com>
2024-11-25 16:32:03 -0600

    Abolish undefined behavior from using go's map directly

--- main.go
+++ main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"net/http"
 )
 
-var receipt_database = make(map[string]*Receipt)
+// XXX: Performing updates to this map would require synchronization
+var receipt_database = myMap{}
+
+type myMap struct {
+	sync.Map
+}
+
+func (m *myMap) Insert(key string, value *Receipt) {
+	m.Store(key, value)
+}
+
+func (m *myMap) Select(key string) *Receipt {
+	found_receipt, _ := m.Load(key)
+	r, _ := found_receipt.(*Receipt)
+	if r != nil {
+		return r
+	} else {
+		return nil
+	}
+}
 
 func getItems(c *gin.Context) {
 	id := c.Param("id")
 
-	if resp := receipt_database[id]; resp != nil {
+	if resp := receipt_database.Select(id); resp != nil {
 		c.JSON(http.StatusOK, receipt_query_success_response{Points: resp.Points})
 	} else {
 		c.AbortWithStatus(http.StatusNotFound)
@@ -23,7 +44,7 @@ func tryInstallReceipt(c *gin.Context) {
 	new_receipt := &ReceiptContent{}
 	if err := c.BindJSON(&new_receipt); err == nil {
 		new_uuid := uuid.NewString()
-		receipt_database[new_uuid] = NewReceipt(new_receipt, new_uuid)
+		receipt_database.Insert(new_uuid, NewReceipt(new_receipt, new_uuid))
 		c.JSON(http.StatusOK, receipt_install_success_response{ID: new_uuid})
 	} else {
 		c.AbortWithStatus(http.StatusBadRequest)
